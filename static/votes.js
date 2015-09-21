4e2d8aa7a91f5ff0a2bfe5d5e9a6ded27761b9ad
Jon Ander Besga <jonan.bsg@gmail.com>
2015-09-21 20:53:22 +0000

    updated

--- static/votes.js
+++ static/votes.js
@@ -3,10 +3,11 @@ $(function() {
   var socket = io();
   
   socket.emit('special socket');
-  
+
   socket.on('update', function (data) {
-      
-      /* global update */
-      update(data.yes, data.no);
+      /* global myBarChart */
+      myBarChart.datasets[0].bars[0].value = data.yes;
+      myBarChart.datasets[0].bars[1].value = data.no;
+      myBarChart.update();
   });
 });
